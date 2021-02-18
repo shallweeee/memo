49fde73df16e06873b27a5625a86d17a742a46de
shallweeee <phcphc82@gmail.com>
2021-02-19 00:17:26 +0900

    Create join2.py

---
+++ python/join2.py
@@ -0,0 +1,21 @@
+import pandas as pd
+
+dic = {}
+for f in 'abcd':
+    with open(f, 'r') as fp:
+        dic[f] = [l.strip() for l in fp.readlines()]
+
+def mkdf(k, v):
+    df = pd.DataFrame({k: v})
+    t = '_' + k
+    df[t] = df[k]
+    df.index = df[t]
+    df.index.name = None
+    df[k] = 'O'
+    return df.drop(t, axis=1)
+
+dfs = [mkdf(k, v) fro k, v in dic.items()]
+df = pd.concat(dfs, axis=1)
+df.fillna('')
+df = df.sort_index()
+df.to_csv('out.csv')
